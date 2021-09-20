9494c94aae6da1f893b925c51c528eef3853eca0
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-09-20 17:55:40 +0200

    display cards

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import FlatList from './flatlist.jsx';
+import flats from '../../data/flats';
+// import SimpleMap from './googlemap'
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      flats: flats
+    };
+
+    console.log(this.state.flats)
   }
 
-  // this.state = {
-  //   flats: []
-  // };
 
   render() {
     return (
       <div>
-        <div className="flat-list">
-          <FlatList />
+        <FlatList flats={this.state.flats} />
+        <div className="map-container">
+         {/* {SimpleMap()} */}
         </div>
-        <div className="map-container"></div>
       </div>
     );
   }
--- src/components/flatlist.jsx
+++ src/components/flatlist.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import Flat from './flat.jsx';
-import flats from '../../data/flats';
 
 class FaltList extends Component {
   constructor(props) {
     super(props);
   }
 
+  renderList = () => {
+  return this.props.flats.map(flat => {
+      return  <Flat flat={flat} key={flat.name} />
+        })
+  }
+
   render() {
     return (
-      <div className="card">
-        <Flat />
+      <div className="flat-list">
+         {this.renderList()}
       </div>
     )
   }
