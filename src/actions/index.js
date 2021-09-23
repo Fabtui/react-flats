b45450910575a9dc068393d3b2c735db46426f36
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-09-23 10:42:36 +0200

    refacto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import flats from '../../data/flats';
 
+export const SET_FLATS = 'SET_FLATS';
+export const SELECT_FLAT = 'SELECT_FLAT';
+
 export function setFlats() {
   // imulate a DB:
   // return {
@@ -21,14 +24,14 @@ export function setFlats() {
   const promise = fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
     .then(response => response.json());
   return {
-    type: 'SET_FLATS',
+    type: SET_FLATS,
     payload: promise
   };
 }
 
 export function selectFlat(flat) {
   return {
-    type: 'SELECT_FLAT',
+    type: SELECT_FLAT,
     payload: flat
   }
 }
--- src/reducers/selected_flat_reducer.js
+++ src/reducers/selected_flat_reducer.js
@@ -1,8 +1,10 @@
+import { SELECT_FLAT } from "../actions";
+
 export default function(state, action) {
   if (state === undefined) {
     return null;
   }
-  if (action.type === 'SELECT_FLAT') {
+  if (action.type === SELECT_FLAT) {
     return action.payload;
   } else {
     return state;
